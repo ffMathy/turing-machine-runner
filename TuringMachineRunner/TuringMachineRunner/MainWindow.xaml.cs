80f184fd4cca787e3ee60428b3a48747e7077df2
ffMathy <mathias.lorenzen@live.com>
2014-09-03 15:48:59 +0200

    initial commit.

---
+++ TuringMachineRunner/TuringMachineRunner/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TuringMachineRunner.Models;
+
+namespace TuringMachineRunner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, Dictionary<char, Instruction>> _instructions;
+        private char[] _buffer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _instructions = new Dictionary<string, Dictionary<char, Instruction>>();
+            _buffer = new char[4096];
+        }
+    }
+}
---
+++ TuringMachineRunner/TuringMachineRunner/Models/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuringMachineRunner.Models
+{
+    class Instruction
+    {
+        public string GoToState { get; set; }
+        public char WriteSymbol { get; set; }
+        public Direction Direction { get; set; }
+    }
+
+    enum Direction {
+        Left,
+        Right,
+        Halt
+    }
+}
